fa206dbf279d785bd44f6ab6bfe3b2710da0438c
sharaseaman <shara.seaman@gmail.com>
2017-10-21 16:35:23 -0500

    items update on myList to dataBase

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -3,35 +3,35 @@ var router = express.Router();
 var pool = require('../modules/pool.js');
 
 
-router.get('/',function(req,res){
+router.get('/', function (req, res) {
     console.log('in router/get on list route');
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         console.log('logged in', req.user);
-    pool.connect(function(connectionError, client, done){
-        if(connectionError) {
-            console.log(connectionError);
-            res.sendStatus(500);
-        } else {
-            var queryString = 'SELECT list.name, array_agg(items.item)AS item FROM list INNER JOIN items ON items.list_id=list.id WHERE list.users_id = $1 GROUP BY list.name';
-            var values = [req.user.id];
-            
-            client.query(queryString, values, function(queryError, resultObj){
-                done();
-                if(queryError) {
-                    console.log(queryError);
-                    res.sendStatus(501);
-                } else {
-                    console.log('list router resultObj.rows', resultObj.rows);
-                    res.send(resultObj.rows);
-                }
-            })
-        }
-    })
+        pool.connect(function (connectionError, client, done) {
+            if (connectionError) {
+                console.log(connectionError);
+                res.sendStatus(500);
+            } else {
+                var queryString = 'SELECT list.name, array_agg(items.item)AS item, array_agg(user_items.complete)AS checkbox FROM list RIGHT JOIN items ON items.list_id=list.id LEFT JOIN user_items ON user_items.items_id=items.id WHERE list.users_id=$1 GROUP BY list.name;';
+                var values = [req.user.id];
+
+                client.query(queryString, values, function (queryError, resultObj) {
+                    done();
+                    if (queryError) {
+                        console.log(queryError);
+                        res.sendStatus(501);
+                    } else {
+                        console.log('list router resultObj.rows', resultObj.rows);
+                        res.send(resultObj.rows);
+                    }
+                })
+            }
+        })
     }
-    else{
+    else {
         console.log('not logged in');
         res.send(false);
-        
+
     }
 });
 
@@ -45,10 +45,11 @@ router.put('/', function (req, res) {
             console.log(connectionError);
             res.sendStatus(501);
         } else {
-            var pQuery = "UPDATE items SET complete=$1 WHERE item=$2";
+            var pQuery = 'SELECT id FROM items WHERE item = $1';
+
             console.log('pQuery', pQuery);
-            
-            var valueArray = [flower.complete, flower.data];
+
+            var valueArray = [flower.data];
             client.query(pQuery, valueArray, function (queryError, resultObj) {
                 done();
                 if (queryError) {
@@ -56,12 +57,25 @@ router.put('/', function (req, res) {
                     res.sendStatus(500);
                 } else {
                     console.log('yaaaaaay', resultObj);
-                    res.sendStatus(202);
-                }
-            });
-        }
+                    var pQuery2 = 'UPDATE user_items SET complete=$3 WHERE items_id=$1 AND users_id=$2';
+                    console.log('pQuery', pQuery);
+
+                    var valueArray2 = [resultObj.rows[0].id, req.user.id, flower.complete];
+                    client.query(pQuery2, valueArray2, function (queryError, resultObj) {
+                        done();
+                        if (queryError) {
+                            console.log(queryError);
+                            res.sendStatus(500);
+                        } else {
+                            console.log('yaaaaaay', resultObj);
+                            res.sendStatus(202);
+                        }
+                    })
+            };
+        })
+    }
+
     })
-
-
 });//end put route
+
 module.exports = router;
